0683bd9a3d0a37cd52376776f6dec7452d16ee97
William Gulian <williamgulian@gmail.com>
2021-04-10 18:28:14 -0400

    more cleanup

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,12 @@ if [ ! -d "dist_pages" ]; then
     git worktree add dist_pages gh-pages
 fi
 
-rm -r dist_pages/*
+rm -r dist_pages/* || echo 'no published files'
 
 cp -r dist/* dist_pages/
 
 cd dist_pages
 
-git status
+git --work-tree=./ status
+git --work-tree=./ add .
+git --work-tree=./ commit -m "Deploy at $(date)"
--- ninja.py
+++
@@ -1,6 +0,0 @@
-from app import asm_ninja, ninja_socketio
-
-if __name__ == '__main__':
-    ninja_socketio.run(asm_ninja, port=5000, debug=True)
-
-
